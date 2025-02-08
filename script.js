0f9f2560f83fbfaca6ef0d27b3ac48c61f116354
Siddharth <siddharthrishabh019@gmail.com>
2025-02-08 14:46:54 +0530

    added delete button

--- script.js
+++ script.js
@@ -4,7 +4,15 @@ let expenseCategory = document.querySelector("#category")
 let btn = document.querySelector("#addExpense")
 let table = document.querySelector("#table")
 let total = document.querySelector("#totalExpense")
-let totalEx =0
+let totalEx =0;
+let expensesList;
+
+if (localStorage.getItem("Expenses")){
+  expensesList= JSON.parse(localStorage.getItem("Expenses"));
+}else{
+  expensesList=[];
+}
+loadExpense();
 function addExpense(){
 
 if (!expenseName.value || !expenseAmount.value || !expenseCategory.value) {
@@ -16,17 +24,33 @@ if (!expenseName.value || !expenseAmount.value || !expenseCategory.value) {
      let nameCell = newRow.insertCell(0);
      let amountCell = newRow.insertCell(1);
      let categoryCell = newRow.insertCell(2);
+     let dltCell = newRow.insertCell(3);
 
-     let expenses = localStorage.setItem("Name", expenseName.value);
-           expenses = localStorage.setItem("Amount", expenseAmount.value);
-           expenses = localStorage.setItem("Category", expenseCategory.value);
+      let expense= {
+        name: expenseName.value,
+        amount: parseFloat(expenseAmount.value),
+        category:expenseCategory.value,
+      };
+      let dltbtn = document.createElement("button");
+      dltbtn.textContent="Remove";
+      dltCell.appendChild(dltbtn);
 
-     nameCell.textContent = expenseName.value;
-     amountCell.textContent = "$"+ parseFloat(expenseAmount.value);
-     categoryCell.textContent = expenseCategory.value;
+      dltbtn.addEventListener("click", dltEx =()=> {
+        let rowIndex= newRow.rowIndex;
+        expensesList.splice(rowIndex,1);
+        table.deleteRow(rowIndex);
+      });
+
+      expensesList.push(expense);
+
+    localStorage.setItem("Expenses", JSON.stringify(expensesList));
+
+     nameCell.textContent = expense.name;
+     amountCell.textContent = "$"+ parseFloat(expense.amount);
+     categoryCell.textContent = expense.category;
      
-     totalEx +=parseFloat(expenseAmount.value);
-     total.textContent=  totalEx;
+      totalEx += parseFloat(expenseAmount.value);
+      total.textContent = totalEx;
      
      expenseName.value= "";
      expenseAmount.value= "";
@@ -34,10 +58,35 @@ if (!expenseName.value || !expenseAmount.value || !expenseCategory.value) {
 
 }
 
-btn.addEventListener("click", addExpense)
+btn.addEventListener("click", addExpense);
 document.addEventListener("keydown", function(event){
   if(event.key === "Enter"){
     addExpense();
   }
 });
 
+function loadExpense(){
+  expensesList.forEach(expense => {
+    const newRow = table.insertRow();
+     let nameCell = newRow.insertCell(0);
+     let amountCell = newRow.insertCell(1);
+     let categoryCell = newRow.insertCell(2);
+     let dltCell = newRow.insertCell(3);
+
+     nameCell.textContent = expense.name;
+     amountCell.textContent = "$"+ expense.amount;
+     categoryCell.textContent = expense.category;
+     let dltbtn = document.createElement("button");
+      dltbtn.textContent="Remove";
+      dltCell.appendChild(dltbtn);
+
+      dltbtn.addEventListener("click", dltEx =()=> {
+        let rowIndex= newRow.rowIndex;
+        expensesList.splice(rowIndex,1);
+        table.deleteRow(rowIndex);
+      });
+      totalEx += parseFloat(expense.amount);
+      total.textContent = totalEx;
+
+  });
+}
